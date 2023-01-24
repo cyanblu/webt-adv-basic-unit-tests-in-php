711659fb41eb0e0eebdd33af5ad39a99f51362f1
ChristianWidauer <9198@htl.rennweg.at>
2023-01-24 15:43:06 +0100

    uploads

---
+++ InterfaceVideo.php
@@ -0,0 +1,9 @@
+<?php
+
+interface InterfaceVideo
+{
+    public function getName();
+    public function getSource();
+    public function getHtmlCode();
+    public function getTime();
+}
---
+++ Youtube.php
@@ -0,0 +1,43 @@
+<?php
+
+class Youtube implements InterfaceVideo
+{
+    private string $name;
+    private string $source;
+    private string $htmlCode;
+    private string $time;
+
+    /**
+     * @param string $name
+     * @param string $source
+     * @param string $htmlCode
+     */
+    public function __construct(string $name, string $source, string $htmlCode, string $time)
+    {
+        $this->name = $name;
+        $this->source = $source;
+        $this->htmlCode = $htmlCode;
+        $this->time = $time;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    public function getHtmlCode()
+    {
+        return $this->htmlCode;
+    }
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+}
